aa11c033f91e810598ce203797062b5482556d2e
Exter-dg <prkandmap23@rocketmail.com>
2023-03-20 14:18:35 +0530

    Add: Navbar UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "./components/user/Navbar";
 
-export default function App () {
-	return <div>App</div>;
+export default function App() {
+	return (
+		<div>
+			<Navbar></Navbar>
+		</div>
+	);
 }
